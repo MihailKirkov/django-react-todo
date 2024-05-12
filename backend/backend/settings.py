074e53f6daebce75b9a9a782496fffcd79ab82d0
Mihail Kirkov <mihailkirkov04@gmail.com>
2024-05-12 18:48:05 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)^bxtmn*z@u9&olaz92fn_jv89ye3sllu6@9uex2xvyw-#q03x'
+SECRET_KEY = 'MY-SECRET-KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
